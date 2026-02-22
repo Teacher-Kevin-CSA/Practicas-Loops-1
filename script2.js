4ac238c4bf90a07231cb9f84a03ba2912b25b7f2
Kevin Emanuel Hernandez Chavez <kevin.hernandez@adoc.superate.org.sv>
2026-02-22 15:46:16 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,52 @@
+// CUENTA REGRESIVA - USANDO WHILE
+
+// 1. Obtén el botón usando su id "generateBtn".
+// 2. Obtén el contenedor donde aparecerán los números (id: "numbers").
+// 3. Asigna un evento click al botón.
+// 4. Crea una función llamada countdown.
+
+// Dentro de la función:
+
+// 6. Crea una variable llamada i y asígnale el valor 10.
+// 7. Crea un ciclo while que se ejecute mientras i sea mayor o igual a 1.
+// 8. Dentro del ciclo:
+//      - Crea un nuevo elemento div.
+//      - Asigna como texto el valor de i.
+//      - Agrega la clase "number-box" al div.
+//      - Agrega el div al contenedor.
+//      - Disminuye i en 1.
+// 9. Cierra el ciclo.
+
+// Limpia el contenedor para eliminar números anteriores.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DESAFÍO:
+
+// Modifica el ciclo para que:
+// - Los números pares tengan un color diferente.
+// - Los números impares tengan otro color.
+// (Necesitarás usar una estructura if dentro del ciclo.)
