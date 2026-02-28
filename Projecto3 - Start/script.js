c0435314a6e78dca3a59c80c213d1224c15d9b94
Kevin Emanuel Hernandez Chavez <kevin.hernandez@adoc.superate.org.sv>
2026-02-27 20:35:53 -0600

    Update script.js

--- Projecto3 - Start/script.js
+++ Projecto3 - Start/script.js
@@ -1,3 +1,5 @@
+// GENERADOR DE CAJAS. Revisar HTML y CSS. Las cajitas se generan con estilo que está en el CSS
+
 // 1. Obtener el botón (id: generateBtn).
 // 2. Obtener el contenedor (id: boxes).
 // 3. Asignar evento click al botón.
@@ -8,5 +10,5 @@
 // 6. Crear un ciclo for que se repita 20 veces.
 // 7. En cada vuelta:
 //      - Crear un div.
-//      - Agregar la clase "box".
+//      - Agregar la clase "box". (lo hacen con nombre_de_la_variable.classList.add("box"); la variable donde está el div que crearon arriba
 //      - Agregar el div al contenedor.
